fd6a13bc3fd29eef5e4e85988ddd0cc0135f70a1
Eule1234 <gretenerlena@gmail.com>
2026-08-08 17:34:36 +0200

    feat(app): improve data loading and status handling logic

--- app/script.js
+++ app/script.js
@@ -13,8 +13,11 @@ async function loadData() {
                 document.getElementById('tem-c').textContent = bme680.temp_c + ' °C';
                 document.getElementById('hum-pct').textContent = bme680.hum_pct + ' %';
 
+                getStatus(bme680.temp_c, bme680.hum_pct);
+
                 valuesDiv.classList.remove('invisible');
                 errorMessageDiv.classList.add('invisible');
+
             } catch (error) {
                 card.innerHTML = '<p class="error">Fehler: Daten konnten nicht geladen werden.</p>';
                 console.error(error);
@@ -25,40 +28,40 @@ async function loadData() {
     loadData();
 });
 
+
+
+
+    const EDB_Temp_Gut_Min = 20;
+    const EDB_Temp_Gut_Max = 24;
+    const EDB_Temp_Kritisch_Min = 10;
+    const EDB_Temp_Kritisch_Max = 20.;
+    const EDB_Hum_Gut_Min = 30;
+    const EDB_Hum_Gut_Max = 40;
+    const EDB_Hum_Kritisch_Min = 10;
+    const EDB_Hum_Kritisch_Max = 30;
+
+    function getStatus(temp_c, hum_pct) {
     const StatusGutDiv = document.getElementById('status-gut');
     StatusGutDiv.classList.add('invisible');
     const StatusKritischDiv = document.getElementById('status-kritisch');
     StatusKritischDiv.classList.add('invisible');
     const StatusSchlechtDiv = document.getElementById('status-schlecht');
     StatusSchlechtDiv.classList.add('invisible');
-    const errorMessageDiv = document.getElementById('message');
-    errorMessageDiv.classList.remove('invisible');
-    const EDB_Temp_Gut_Min = 20;
-    const EDB_Temp_Gut_Max = 24;
-    const EDB_Temp_Kritisch_Min = 10;
-    const EDB_Temp_Kritisch_Max = 19;
-    const EDB_Temp_Schlecht_Min = 25;
-    const EDB_Temp_Schlecht_Max = 40;
-    const EDB_Hum_Gut_Min = 30;
-    const EDB_Hum_Gut_Max = 40;
-    const EDB_Hum_Kritisch_Min = 10;
-    const EDB_Hum_Kritisch_Max = 29;
-    const EDB_Hum_Schlecht_Min = 41;
-    const EDB_Hum_Schlecht_Max = 80;
 
-        function getStatus(temp_c, hum_pct) {
-            const tempGut = temp_c >= EDB_Temp_Gut_Min && tempC <= EDB_Temp_Gut_Max;
-            const humGut = hum_pct >= EDB_Hum_Gut_Min && hum_pct <= EDB_Hum_Gut_Max;
-            const tempKritisch = temp_c >= EDB_Temp_Kritisch_Min && temp_c <= EDB_Temp_Kritisch_Max;
-            const humKritisch = hum_pct >= EDB_Hum_Kritisch_Min && hum_pct <= EDB_Hum_Kritisch_Max;
-            const tempSchlecht = temp_c >= EDB_Temp_Schlecht_Min && temp_c <= EDB_Temp_Schlecht_Max;
-            const humSchlecht = hum_pct >= EDB_Hum_Schlecht_Min && hum_pct <= EDB_Hum_Schlecht_Max;
+    const tempGut = temp_c >= EDB_Temp_Gut_Min && temp_c <= EDB_Temp_Gut_Max;
+    const humGut = hum_pct >= EDB_Hum_Gut_Min && hum_pct <= EDB_Hum_Gut_Max;
+    const tempKritisch = temp_c >= EDB_Temp_Kritisch_Min && temp_c <= EDB_Temp_Kritisch_Max;
+    const humKritisch = hum_pct >= EDB_Hum_Kritisch_Min && hum_pct <= EDB_Hum_Kritisch_Max;
 
-            if (tempGut && humGut) return StatusGutDiv.classList.remove('invisible') ;
-            else if (tempKritisch && humKritisch) return StatusKritischDiv.classList.remove('invisible');
-            else if (tempSchlecht && humSchlecht) return StatusSchlechtDiv.classList.remove('invisible');
-            else return 'nicht erkannt';
-        }
-        window.addEventListener('DOMContentLoaded', function() {
-        getStatus();
-})
+
+    if (tempGut && humGut) {
+        return StatusGutDiv.classList.remove('invisible');
+    }
+    else if (tempKritisch && humKritisch) {
+        return StatusKritischDiv.classList.remove('invisible');
+    }
+    else {
+        return StatusSchlechtDiv.classList.remove('invisible');
+    }
+
+}
