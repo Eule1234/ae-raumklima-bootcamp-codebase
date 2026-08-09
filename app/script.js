9a15e79bc0fec6faaf39bb4a469fb35f88d03b8d
Eule1234 <gretenerlena@gmail.com>
2026-08-09 17:19:56 +0200

    feat(app): display of history

--- app/script.js
+++ app/script.js
@@ -8,7 +8,7 @@ async function loadData() {
                 const response = await fetch('data.json');
                 const bundles = await response.json();
 
-                const data = bundles[bundles.length - 1]; 
+                const data = bundles[0]; 
                 const bme680 = data.readings.bme680;
                 document.getElementById('tem-c').textContent = bme680.temp_c + ' °C';
                 document.getElementById('hum-pct').textContent = bme680.hum_pct + ' %';
@@ -19,7 +19,7 @@ async function loadData() {
                 errorMessageDiv.classList.add('invisible');
 
             } catch (error) {
-                card.innerHTML = '<p class="error">Fehler: Daten konnten nicht geladen werden.</p>';
+                errorMessageDiv.innerHTML = '<p class="error">Fehler: Daten konnten nicht geladen werden.</p>';
                 console.error(error);
             }
     }
@@ -34,7 +34,7 @@ async function loadData() {
     const EDB_Temp_Gut_Min = 20;
     const EDB_Temp_Gut_Max = 24;
     const EDB_Temp_Kritisch_Min = 10;
-    const EDB_Temp_Kritisch_Max = 20.;
+    const EDB_Temp_Kritisch_Max = 20;
     const EDB_Hum_Gut_Min = 30;
     const EDB_Hum_Gut_Max = 40;
     const EDB_Hum_Kritisch_Min = 10;
@@ -65,3 +65,54 @@ async function loadData() {
     }
 
 }
+
+async function loadDashboard() {
+    try {
+        const response = await fetch('data.json');
+        const bundles = await response.json();
+
+        renderHistory(bundles);
+
+    } catch (error) {
+        const errorMessageDiv = document.getElementById('message');
+        errorMessageDiv.innerHTML = '<p class="error">Fehler: Daten konnten nicht geladen werden.</p>';
+        console.error(error);
+    }
+        loadDashboard();
+}
+
+function renderHistory(bundles) {
+    const container = document.getElementById('history-list'); 
+    if (!container) return;
+
+    container.innerHTML = "";
+
+    const limitedBundles = bundles.slice(0, 10);
+
+    limitedBundles.forEach(bundle => {
+        const item = document.createElement('div');
+        item.classList.add('history-item');
+
+        const formattedDate = new Date(bundle.recorded_at).toLocaleString('de-DE', {
+            day: '2-digit', month: '2-digit', hour: '2-digit', minute: '2-digit'
+        });
+
+        const temp_c = bundle.readings?.bme680?.temp_c ?? '--';
+        const hum_pct = bundle.readings?.bme680?.hum_pct ?? '--';
+
+        item.innerHTML = `
+        <div id="history-time">
+             <small class="item-meta">${formattedDate} Uhr</small>
+        </div>
+        <div id="history-temp">
+            <span>${temp_c}°C</span>
+        </div>
+        <div id="history-hum">
+            <span>${hum_pct}%</span>
+        </div>
+        `;
+
+        container.appendChild(item);
+    });
+}
+
